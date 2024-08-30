c3a92eb3f57d3c59789a4b5cb2220d5d5b30b0cb
turbobeest <github@terbeest.com>
2024-08-30 08:09:04 -0400

    v1.2

--- src/codebase_extractor.py
+++ src/codebase_extractor.py
@@ -1,22 +1,26 @@
 """ Codebase and Codemap Extractor
 
-This script is designed to create a comprehensive snapshot of a software project,
-facilitating the training of Large Language Models (LLMs) on codebases. It analyzes
-a specified directory, generates detailed codemaps and extracting the codebase while
-preserving its structure. The tool also examines and documents imported libraries,
-providing insights into the project's dependencies.
+This script is designed to create a comprehensive snapshot of a 
+software project for visibility with Large Language Model (LLM) 
+development support with codebases. It analyzes a specified directory, 
+generates a detailed codemap and extracts the codebase to a single 
+directory (absent any subdirectories). The tool also examines and 
+documents imported libraries, providing insights into the project's 
+dependencies and produces codebases and codemaps of each library 
+(default is none).
 
 Key features include:
-- Interactive selection of directories, file types, and libraries to include
-- Generation of hierarchical codemaps showing the project structure
-- Extraction of the codebase with timestamps for each file
-- Analysis and documentation of imported libraries, including versions and file counts
-- Flexible output options for codemaps, codebases, and library information
 
-By capturing the essence of a project's structure, contents, and dependencies,
-this tool creates a rich dataset ideal for training LLMs to understand and work
-with complex software projects. It aims to enhance an LLM's ability to comprehend
-code organization, dependencies, and project architectures."""
+List of ignored file types and directories to exclude from analysis
+Generation of hierarchical codemap showing the project structure
+Extraction of the codebase with timestamps
+Analysis and documentation of imported libraries
+
+By capturing the essence of a project's structure, contents, and 
+dependencies, this tool creates a rich dataset ideal for informing 
+LLMs to understand and work with complex software projects. It 
+aims to enhance an LLM's ability to comprehend code organization, 
+dependencies, and project architectures"""
 
 import os
 import json
